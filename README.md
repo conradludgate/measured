2c08eaef30c2a943c892f4850ea3f3d4ea59c26f
Conrad Ludgate <conradludgate@gmail.com>
2024-02-04 13:28:22 +0000

    add high cardinality bench

--- README.md
+++ README.md
@@ -18,41 +18,76 @@ A better\* metrics crate
 ## Benchmark results
 
 The benchmark runs on multiple threads a simple counter increment, with various labels,
-and then samples+encodes the values into the Prometheus text format. This crate outperforms both metrics and prometheus
+and then samples/encodes the values into the Prometheus text format. This crate outperforms both `metrics` and `prometheus`
 when it comes to both speed and allocations.
+
+The `fixed_cardinality` group has only 2 label pairs and 18 distinct label groups.
+The `high_cardinality` group has 3 label pairs and 18,000 distinct label groups.
 
 ### Macbook Pro M2 Max (12 threads):
 
 ```
 Timer precision: 41 ns
-counters       fastest       │ slowest       │ median        │ mean          │ samples │ iters
-├─ measured    11.52 ms      │ 17.77 ms      │ 16.82 ms      │ 16.53 ms      │ 504     │ 2520
-│              alloc:        │               │               │               │         │
-│                0           │ 2             │ 0             │ 0.162         │         │
-│                0 B         │ 48 B          │ 0 B           │ 3.904 B       │         │
-│              grow:         │               │               │               │         │
-│                0           │ 7             │ 0             │ 0.569         │         │
-│                0 B         │ 1.656 KB      │ 0 B           │ 134.7 B       │         │
-├─ metrics     35.08 ms      │ 46.11 ms      │ 38.78 ms      │ 38.93 ms      │ 504     │ 2520
-│              alloc:        │               │               │               │         │
-│                0           │ 0             │ 0             │ 6024          │         │
-│                0 B         │ 0 B           │ 0 B           │ 385.3 KB      │         │
-│              dealloc:      │               │               │               │         │
-│                0           │ 0             │ 0             │ 6024          │         │
-│                0 B         │ 0 B           │ 0 B           │ 385.4 KB      │         │
-│              grow:         │               │               │               │         │
-│                0           │ 0             │ 0             │ 5.083         │         │
-│                0 B         │ 0 B           │ 0 B           │ 219.5 B       │         │
-╰─ prometheus  118 ms        │ 143.2 ms      │ 137.5 ms      │ 136.9 ms      │ 504     │ 2520
-               alloc:        │               │               │               │         │
-                 0           │ 0             │ 0             │ 11.33         │         │
-                 0 B         │ 0 B           │ 0 B           │ 758.4 B       │         │
-               dealloc:      │               │               │               │         │
-                 0           │ 0             │ 0             │ 11.25         │         │
-                 0 B         │ 0 B           │ 0 B           │ 757.7 B       │         │
-               grow:         │               │               │               │         │
-                 0           │ 0             │ 0             │ 0.583         │         │
-                 0 B         │ 0 B           │ 0 B           │ 138 B         │         │
+counters              fastest       │ slowest       │ median        │ mean          │ samples │ iters
+├─ fixed_cardinality                │               │               │               │         │
+│  ├─ measured        18.19 ms      │ 27.91 ms      │ 27.07 ms      │ 26.48 ms      │ 504     │ 2520
+│  │                  alloc:        │               │               │               │         │
+│  │                    0           │ 0             │ 0             │ 0.162         │         │
+│  │                    0 B         │ 0 B           │ 0 B           │ 3.904 B       │         │
+│  │                  grow:         │               │               │               │         │
+│  │                    0           │ 0             │ 0             │ 0.569         │         │
+│  │                    0 B         │ 0 B           │ 0 B           │ 134.7 B       │         │
+│  ├─ metrics         32.02 ms      │ 39.51 ms      │ 36.89 ms      │ 36.85 ms      │ 504     │ 2520
+│  │                  alloc:        │               │               │               │         │
+│  │                    0           │ 0             │ 0             │ 6024          │         │
+│  │                    0 B         │ 0 B           │ 0 B           │ 385.3 KB      │         │
+│  │                  dealloc:      │               │               │               │         │
+│  │                    0           │ 0             │ 0             │ 6024          │         │
+│  │                    0 B         │ 0 B           │ 0 B           │ 385.4 KB      │         │
+│  │                  grow:         │               │               │               │         │
+│  │                    0           │ 0             │ 0             │ 5.083         │         │
+│  │                    0 B         │ 0 B           │ 0 B           │ 219.5 B       │         │
+│  ╰─ prometheus      112.4 ms      │ 143.1 ms      │ 136.3 ms      │ 135.3 ms      │ 504     │ 2520
+│                     alloc:        │               │               │               │         │
+│                       0           │ 0             │ 59            │ 9.833         │         │
+│                       0 B         │ 0 B           │ 3.294 KB      │ 549 B         │         │
+│                     dealloc:      │               │               │               │         │
+│                       0           │ 0             │ 58.4          │ 9.75          │         │
+│                       0 B         │ 0 B           │ 3.29 KB       │ 548.4 B       │         │
+│                     grow:         │               │               │               │         │
+│                       0           │ 0             │ 3.4           │ 0.583         │         │
+│                       0 B         │ 0 B           │ 828 B         │ 138 B         │         │
+╰─ high_cardinality                 │               │               │               │         │
+   ├─ measured        1.305 s       │ 3.213 s       │ 1.917 s       │ 2.108 s       │ 24      │ 48
+   │                  alloc:        │               │               │               │         │
+   │                    0           │ 0             │ 0             │ 3.083         │         │
+   │                    0 B         │ 0 B           │ 0 B           │ 285.7 KB      │         │
+   │                  dealloc:      │               │               │               │         │
+   │                    0           │ 0             │ 0             │ 2.916         │         │
+   │                    0 B         │ 0 B           │ 0 B           │ 142.8 KB      │         │
+   │                  grow:         │               │               │               │         │
+   │                    0           │ 0             │ 0             │ 1.854         │         │
+   │                    0 B         │ 0 B           │ 0 B           │ 6.247 MB      │         │
+   ├─ metrics         1.3 s         │ 5.652 s       │ 2.988 s       │ 3.153 s       │ 24      │ 48
+   │                  alloc:        │               │               │               │         │
+   │                    0           │ 0             │ 0             │ 939753        │         │
+   │                    0 B         │ 0 B           │ 0 B           │ 41.07 MB      │         │
+   │                  dealloc:      │               │               │               │         │
+   │                    0           │ 0             │ 0             │ 933848        │         │
+   │                    0 B         │ 0 B           │ 0 B           │ 44.17 MB      │         │
+   │                  grow:         │               │               │               │         │
+   │                    0           │ 0             │ 0             │ 210875        │         │
+   │                    0 B         │ 0 B           │ 0 B           │ 9.677 MB      │         │
+   ╰─ prometheus      1.323 s       │ 6.181 s       │ 2.987 s       │ 3.344 s       │ 24      │ 48
+                      alloc:        │               │               │               │         │
+                        0           │ 0             │ 0             │ 375835        │         │
+                        0 B         │ 0 B           │ 0 B           │ 19.14 MB      │         │
+                      dealloc:      │               │               │               │         │
+                        0           │ 0             │ 0             │ 355183        │         │
+                        0 B         │ 0 B           │ 0 B           │ 18.44 MB      │         │
+                      grow:         │               │               │               │         │
+                        0           │ 0             │ 0             │ 4427          │         │
+                        0 B         │ 0 B           │ 0 B           │ 6.32 MB       │         │
 ```
 
 ### Ryzen 9 7950X (32 threads):
--- core/benches/counters.rs
+++ core/benches/counters.rs
@@ -1,132 +1,306 @@
-use divan::{black_box, AllocProfiler, Bencher, Divan};
-use lasso::{Rodeo, RodeoReader};
-use measured_derive::{FixedCardinalityLabel, LabelGroup};
+use divan::{AllocProfiler, Divan};
 
 #[global_allocator]
 static ALLOC: AllocProfiler = AllocProfiler::system();
-
-const LOOPS: usize = 2000;
 
 fn main() {
     Divan::from_args().threads([0]).run_benches();
 }
 
-#[divan::bench(sample_size = 5, sample_count = 500)]
-fn measured(bencher: Bencher) {
-    use measured::metric::name::{MetricName, Total};
+mod fixed_cardinality {
+    use divan::{black_box, Bencher};
+    use lasso::{Rodeo, RodeoReader};
+    use measured_derive::{FixedCardinalityLabel, LabelGroup};
 
-    let error_set = ErrorsSet {
-        route: Rodeo::from_iter(routes()).into_reader(),
-    };
-    let counter_vec = measured::CounterVec::new_counter_vec(error_set);
+    const LOOPS: usize = 2000;
 
-    bencher
-        .with_inputs(measured::text::TextEncoder::new)
-        .bench_refs(|encoder| {
+    #[divan::bench(sample_size = 5, sample_count = 500)]
+    fn measured(bencher: Bencher) {
+        use measured::metric::name::{MetricName, Total};
+
+        let error_set = ErrorsSet {
+            route: Rodeo::from_iter(routes()).into_reader(),
+        };
+        let counter_vec = measured::CounterVec::new_counter_vec(error_set);
+
+        bencher
+            .with_inputs(measured::text::TextEncoder::new)
+            .bench_refs(|encoder| {
+                for _ in 0..black_box(LOOPS) {
+                    for &kind in errors() {
+                        for route in routes() {
+                            counter_vec.inc(Error { kind, route })
+                        }
+                    }
+                }
+
+                let metric = "http_request_errors".with_suffix(Total);
+                encoder.write_help(&metric, "help text");
+                counter_vec.collect_into(&metric, encoder);
+                encoder.finish();
+            });
+    }
+
+    #[divan::bench(sample_size = 5, sample_count = 500)]
+    fn prometheus(bencher: Bencher) {
+        let registry = prometheus::Registry::new();
+        let counter_vec = prometheus::register_int_counter_vec_with_registry!(
+            "http_request_errors",
+            "help text",
+            &["kind", "route"],
+            registry
+        )
+        .unwrap();
+
+        bencher.with_inputs(String::new).bench_refs(|string| {
             for _ in 0..black_box(LOOPS) {
                 for &kind in errors() {
                     for route in routes() {
-                        counter_vec.inc(Error { kind, route })
+                        counter_vec.with_label_values(&[kind.to_str(), route]).inc()
                     }
                 }
             }
 
-            let metric = "http_request_errors".with_suffix(Total);
-            encoder.write_help(&metric, "help text");
-            counter_vec.collect_into(&metric, encoder);
-            encoder.finish();
+            string.clear();
+            prometheus::TextEncoder
+                .encode_utf8(&registry.gather(), string)
+                .unwrap();
         });
-}
+    }
 
-#[divan::bench(sample_size = 5, sample_count = 500)]
-fn prometheus(bencher: Bencher) {
-    let registry = prometheus::Registry::new();
-    let counter_vec = prometheus::register_int_counter_vec_with_registry!(
-        "http_request_errors",
-        "help text",
-        &["kind", "route"],
-        registry
-    )
-    .unwrap();
+    #[divan::bench(sample_size = 5, sample_count = 500)]
+    fn metrics(bencher: Bencher) {
+        let recorder = metrics_exporter_prometheus::PrometheusBuilder::new().build_recorder();
 
-    bencher.with_inputs(String::new).bench_refs(|string| {
-        for _ in 0..black_box(LOOPS) {
-            for &kind in errors() {
-                for route in routes() {
-                    counter_vec.with_label_values(&[kind.to_str(), route]).inc()
+        metrics::with_local_recorder(&recorder, || {
+            metrics::describe_counter!("http_request_errors", "help text")
+        });
+
+        bencher.bench(|| {
+            metrics::with_local_recorder(&recorder, || {
+                for _ in 0..black_box(LOOPS) {
+                    for &kind in errors() {
+                        for route in routes() {
+                            let labels = [("kind", kind.to_str()), ("route", route)];
+                            metrics::counter!("http_request_errors", &labels).increment(1);
+                        }
+                    }
                 }
+            });
+
+            recorder.handle().render()
+        });
+    }
+
+    fn routes() -> &'static [&'static str] {
+        black_box(&[
+            "/api/v1/users",
+            "/api/v1/users/:id",
+            "/api/v1/products",
+            "/api/v1/products/:id",
+            "/api/v1/products/:id/owner",
+            "/api/v1/products/:id/purchase",
+        ])
+    }
+
+    fn errors() -> &'static [ErrorKind] {
+        black_box(&[ErrorKind::User, ErrorKind::Internal, ErrorKind::Network])
+    }
+
+    #[derive(Clone, Copy, PartialEq, Debug, LabelGroup)]
+    #[label(set = ErrorsSet)]
+    struct Error<'a> {
+        #[label(fixed)]
+        kind: ErrorKind,
+        #[label(fixed_with = RodeoReader)]
+        route: &'a str,
+    }
+
+    #[derive(Clone, Copy, PartialEq, Debug, FixedCardinalityLabel)]
+    #[label(rename_all = "kebab-case")]
+    enum ErrorKind {
+        User,
+        Internal,
+        Network,
+    }
+
+    impl ErrorKind {
+        fn to_str(self) -> &'static str {
+            match self {
+                ErrorKind::User => "user",
+                ErrorKind::Internal => "internal",
+                ErrorKind::Network => "network",
             }
         }
-
-        string.clear();
-        prometheus::TextEncoder
-            .encode_utf8(&registry.gather(), string)
-            .unwrap();
-    });
+    }
 }
 
-#[divan::bench(sample_size = 5, sample_count = 500)]
-fn metrics(bencher: Bencher) {
-    let recorder = metrics_exporter_prometheus::PrometheusBuilder::new().build_recorder();
+mod high_cardinality {
+    use std::sync::atomic::{AtomicU64, Ordering};
 
-    metrics::with_local_recorder(&recorder, || {
-        metrics::describe_counter!("http_request_errors", "help text")
-    });
+    use divan::{black_box, Bencher};
+    use fake::{faker::name::raw::Name, locales::EN, Fake};
+    use lasso::{Rodeo, RodeoReader, ThreadedRodeo};
+    use measured_derive::{FixedCardinalityLabel, LabelGroup};
+    use metrics::SharedString;
+    use rand::{rngs::StdRng, SeedableRng};
 
-    bencher.bench(|| {
-        metrics::with_local_recorder(&recorder, || {
-            for _ in 0..black_box(LOOPS) {
-                for &kind in errors() {
-                    for route in routes() {
-                        let labels = [("kind", kind.to_str()), ("route", route)];
-                        metrics::counter!("http_request_errors", &labels).increment(1);
+    const LOOPS: usize = 1000;
+
+    fn get_names(thread: &AtomicU64) -> Vec<String> {
+        let extra = errors().len() * routes().len();
+        let mut rng = StdRng::seed_from_u64(thread.fetch_add(1, Ordering::AcqRel));
+        std::iter::repeat_with(|| Name(EN).fake_with_rng::<String, StdRng>(&mut rng))
+            .take(LOOPS * extra)
+            .collect()
+    }
+
+    #[divan::bench(sample_size = 2, sample_count = 20)]
+    fn measured(bencher: Bencher) {
+        use measured::metric::name::{MetricName, Total};
+
+        let error_set = ErrorsSet {
+            route: Rodeo::from_iter(routes()).into_reader(),
+            user_name: ThreadedRodeo::new(),
+        };
+        let counter_vec = measured::CounterVec::new_counter_vec(error_set);
+
+        let thread = AtomicU64::new(0);
+
+        bencher
+            .with_inputs(|| (measured::text::TextEncoder::new(), get_names(&thread)))
+            .bench_refs(|(encoder, names)| {
+                let mut names = names.iter();
+                for _ in 0..black_box(LOOPS) {
+                    for &kind in errors() {
+                        for route in routes() {
+                            counter_vec.inc(Error {
+                                kind,
+                                route,
+                                user_name: names.next().unwrap(),
+                            })
+                        }
                     }
                 }
-            }
+
+                let metric = "http_request_errors".with_suffix(Total);
+                encoder.write_help(&metric, "help text");
+                counter_vec.collect_into(&metric, encoder);
+                encoder.finish();
+            });
+    }
+
+    #[divan::bench(sample_size = 2, sample_count = 20)]
+    fn prometheus(bencher: Bencher) {
+        let registry = prometheus::Registry::new();
+        let counter_vec = prometheus::register_int_counter_vec_with_registry!(
+            "http_request_errors_total",
+            "help text",
+            &["kind", "route", "user_name"],
+            registry
+        )
+        .unwrap();
+
+        let thread = AtomicU64::new(0);
+
+        bencher
+            .with_inputs(|| (String::new(), get_names(&thread)))
+            .bench_refs(|(string, names)| {
+                let mut names = names.iter();
+                for _ in 0..black_box(LOOPS) {
+                    for &kind in errors() {
+                        for route in routes() {
+                            counter_vec
+                                .with_label_values(&[kind.to_str(), route, &names.next().unwrap()])
+                                .inc()
+                        }
+                    }
+                }
+
+                string.clear();
+                prometheus::TextEncoder
+                    .encode_utf8(&registry.gather(), string)
+                    .unwrap();
+            });
+    }
+
+    #[divan::bench(sample_size = 2, sample_count = 20)]
+    fn metrics(bencher: Bencher) {
+        let recorder = metrics_exporter_prometheus::PrometheusBuilder::new().build_recorder();
+
+        metrics::with_local_recorder(&recorder, || {
+            metrics::describe_counter!("http_request_errors", "help text")
         });
 
-        recorder.handle().render()
-    });
-}
+        let thread = AtomicU64::new(0);
 
-fn routes() -> &'static [&'static str] {
-    black_box(&[
-        "/api/v1/users",
-        "/api/v1/users/:id",
-        "/api/v1/products",
-        "/api/v1/products/:id",
-        "/api/v1/products/:id/owner",
-        "/api/v1/products/:id/purchase",
-    ])
-}
+        bencher
+            .with_inputs(|| get_names(&thread))
+            .bench_refs(|names| {
+                let mut names = names.iter();
+                metrics::with_local_recorder(&recorder, || {
+                    for _ in 0..black_box(LOOPS) {
+                        for &kind in errors() {
+                            for route in routes() {
+                                let labels = [
+                                    ("kind", SharedString::const_str(kind.to_str())),
+                                    ("route", SharedString::const_str(route)),
+                                    (
+                                        "user_name",
+                                        SharedString::from_owned(names.next().unwrap().to_owned()),
+                                    ),
+                                ];
+                                metrics::counter!("http_request_errors", &labels).increment(1);
+                            }
+                        }
+                    }
+                });
 
-fn errors() -> &'static [ErrorKind] {
-    black_box(&[ErrorKind::User, ErrorKind::Internal, ErrorKind::Network])
-}
+                recorder.handle().render()
+            });
+    }
 
-#[derive(Clone, Copy, PartialEq, Debug, LabelGroup)]
-#[label(set = ErrorsSet)]
-struct Error<'a> {
-    #[label(fixed)]
-    kind: ErrorKind,
-    #[label(fixed_with = RodeoReader)]
-    route: &'a str,
-}
+    fn routes() -> &'static [&'static str] {
+        black_box(&[
+            "/api/v1/users",
+            "/api/v1/users/:id",
+            "/api/v1/products",
+            "/api/v1/products/:id",
+            "/api/v1/products/:id/owner",
+            "/api/v1/products/:id/purchase",
+        ])
+    }
 
-#[derive(Clone, Copy, PartialEq, Debug, FixedCardinalityLabel)]
-#[label(rename_all = "kebab-case")]
-enum ErrorKind {
-    User,
-    Internal,
-    Network,
-}
+    fn errors() -> &'static [ErrorKind] {
+        black_box(&[ErrorKind::User, ErrorKind::Internal, ErrorKind::Network])
+    }
 
-impl ErrorKind {
-    fn to_str(self) -> &'static str {
-        match self {
-            ErrorKind::User => "user",
-            ErrorKind::Internal => "internal",
-            ErrorKind::Network => "network",
+    #[derive(Clone, Copy, PartialEq, Debug, LabelGroup)]
+    #[label(set = ErrorsSet)]
+    struct Error<'a> {
+        #[label(fixed)]
+        kind: ErrorKind,
+        #[label(fixed_with = RodeoReader)]
+        route: &'a str,
+        #[label(dynamic_with = ThreadedRodeo)]
+        user_name: &'a str,
+    }
+
+    #[derive(Clone, Copy, PartialEq, Debug, FixedCardinalityLabel)]
+    #[label(rename_all = "kebab-case")]
+    enum ErrorKind {
+        User,
+        Internal,
+        Network,
+    }
+
+    impl ErrorKind {
+        fn to_str(self) -> &'static str {
+            match self {
+                ErrorKind::User => "user",
+                ErrorKind::Internal => "internal",
+                ErrorKind::Network => "network",
+            }
         }
     }
 }
