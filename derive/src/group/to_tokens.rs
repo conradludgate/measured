30dbee2374fdb4aaa458a38360f205dedfca8ba2
Conrad Ludgate <conradludgate@gmail.com>
2024-02-04 07:42:10 +0000

    simplify token generation

--- derive/src/group/to_tokens.rs
+++ derive/src/group/to_tokens.rs
@@ -9,12 +9,11 @@ use super::LabelGroup;
 impl ToTokens for LabelGroup {
     fn to_tokens(&self, tokens: &mut TokenStream) {
         let Self {
-            vis,
             krate,
             ident,
             generics,
             sorted_fields,
-            set_ident,
+            ..
         } = self;
 
         let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
@@ -26,6 +25,40 @@ impl ToTokens for LabelGroup {
                 <#ty as #krate::label::LabelValue>::visit(&self.#name, v);
             }
         });
+
+        let label_group_set = Set(self);
+
+        tokens.extend(quote! {
+            #[automatically_derived]
+            impl #impl_generics #krate::label::LabelGroup for #ident #ty_generics #where_clause {
+                fn label_names() -> impl ::std::iter::IntoIterator<Item = &'static ::std::primitive::str> {
+                    [#(#names),*]
+                }
+
+                fn label_values(&self, v: &mut impl #krate::label::LabelVisitor) {
+                    #(#visits)*
+                }
+            }
+
+            #label_group_set
+        })
+    }
+}
+
+struct Set<'a>(&'a LabelGroup);
+
+impl ToTokens for Set<'_> {
+    fn to_tokens(&self, tokens: &mut TokenStream) {
+        let LabelGroup {
+            vis,
+            krate,
+            ident,
+            generics,
+            sorted_fields,
+            set_ident,
+        } = self.0;
+
+        let (_, ty_generics, _) = generics.split_for_impl();
 
         let set_fields = sorted_fields.iter().filter_map(|x| {
             let LabelGroupField {
@@ -56,6 +89,87 @@ impl ToTokens for LabelGroup {
             })
             .collect();
 
+        let cardinality_fns = if dynamics.is_empty() {
+            quote!(
+                fn cardinality(&self) -> Option<usize> {
+                    Some(1usize)
+                        #( .and_then(|x| x.checked_mul(#cardinalities)) )*
+                }
+                type Unique = usize;
+                fn encode_dense(&self, value: Self::Unique) -> Option<usize> {
+                    Some(value)
+                }
+                fn decode_dense(&self, value: usize) -> Self::Group<'_> {
+                    self.decode(&value)
+                }
+            )
+        } else {
+            let dynamics = dynamics.iter().map(|_| quote!(usize));
+            quote!(
+                fn cardinality(&self) -> Option<usize> {
+                    None
+                }
+                type Unique = (usize, #(#dynamics),*);
+
+                fn encode_dense(&self, _value: Self::Unique) -> Option<usize> {
+                    None
+                }
+                fn decode_dense(&self, _value: usize) -> Self::Group<'_> {
+                    unreachable!("does not have a dense encoding")
+                }
+            )
+        };
+
+        let encode_fn = SetEncode {
+            group: self.0,
+            fixed,
+            dynamics,
+            cardinalities: &cardinalities,
+        };
+
+        let decode_fn = SetDecode {
+            group: self.0,
+            fixed,
+            dynamics,
+            cardinalities: &cardinalities,
+        };
+
+        tokens.extend(quote! {
+            #vis struct #set_ident {
+                #(#set_fields)*
+            }
+
+            #[automatically_derived]
+            impl #krate::label::LabelGroupSet for #set_ident {
+                // TODO: fix these generics
+                type Group<'a> = #ident #ty_generics;
+
+                #cardinality_fns
+
+                #encode_fn
+
+                #decode_fn
+            }
+        })
+    }
+}
+
+struct SetEncode<'a> {
+    group: &'a LabelGroup,
+    fixed: &'a [LabelGroupField],
+    dynamics: &'a [LabelGroupField],
+    cardinalities: &'a [TokenStream],
+}
+
+impl ToTokens for SetEncode<'_> {
+    fn to_tokens(&self, tokens: &mut TokenStream) {
+        let Self {
+            group: LabelGroup { krate, .. },
+            fixed,
+            dynamics,
+            cardinalities,
+        } = self;
+
         let fixed_encodes: Vec<TokenStream> = fixed
             .iter()
             .map(|x| {
@@ -84,6 +198,41 @@ impl ToTokens for LabelGroup {
                 }
             })
             .collect();
+
+        tokens.extend(quote! {
+            fn encode(&self, value: Self::Group<'_>) -> Option<Self::Unique> {
+                let mut mul = 1;
+                let mut index = 0;
+
+                #(
+                    index += #fixed_encodes * mul;
+                    mul *= #cardinalities;
+                )*
+
+                Some((
+                    index
+                    #(, #dynamic_encodes)*
+                ))
+            }
+        })
+    }
+}
+
+struct SetDecode<'a> {
+    group: &'a LabelGroup,
+    fixed: &'a [LabelGroupField],
+    dynamics: &'a [LabelGroupField],
+    cardinalities: &'a [TokenStream],
+}
+
+impl ToTokens for SetDecode<'_> {
+    fn to_tokens(&self, tokens: &mut TokenStream) {
+        let Self {
+            group: LabelGroup { krate, .. },
+            fixed,
+            dynamics,
+            cardinalities,
+        } = *self;
 
         let fixed_decodes: Vec<TokenStream> = fixed
             .iter()
@@ -122,98 +271,29 @@ impl ToTokens for LabelGroup {
             })
             .collect();
 
-        let field_names = sorted_fields.iter().map(|x| {
+        let field_names = fixed.iter().chain(dynamics).map(|x| {
             let LabelGroupField { name, .. } = x;
             name
         });
 
-        let cardinality_fns = if dynamics.is_empty() {
-            quote!(
-                fn cardinality(&self) -> Option<usize> {
-                    Some(1usize)
-                        #( .and_then(|x| x.checked_mul(#cardinalities)) )*
-                }
-                type Unique = usize;
-                fn encode_dense(&self, value: Self::Unique) -> Option<usize> {
-                    Some(value)
-                }
-                fn decode_dense(&self, value: usize) -> Self::Group<'_> {
-                    self.decode(&value)
-                }
-            )
-        } else {
-            let dynamics = dynamics.iter().map(|_| quote!(usize));
-            quote!(
-                fn cardinality(&self) -> Option<usize> {
-                    None
-                }
-                type Unique = (usize, #(#dynamics),*);
-
-                fn encode_dense(&self, _value: Self::Unique) -> Option<usize> {
-                    None
-                }
-                fn decode_dense(&self, _value: usize) -> Self::Group<'_> {
-                    unreachable!("does not have a dense encoding")
-                }
-            )
-        };
-
         tokens.extend(quote! {
-            #[automatically_derived]
-            impl #impl_generics #krate::label::LabelGroup for #ident #ty_generics #where_clause {
-                fn label_names() -> impl ::std::iter::IntoIterator<Item = &'static ::std::primitive::str> {
-                    [#(#names),*]
-                }
+            fn decode(&self, value: &Self::Unique) -> Self::Group<'_> {
+                let (index #(, #dynamic_indices)*) = *value;
 
-                fn label_values(&self, v: &mut impl #krate::label::LabelVisitor) {
-                    #(#visits)*
-                }
-            }
+                #(
+                    let card = #cardinalities;
+                    let (index, index1) = (index / card, index % card);
+                    #fixed_decodes;
+                )*
 
-            #vis struct #set_ident {
-                #(#set_fields)*
-            }
+                debug_assert_eq!(index, 0);
 
-            #[automatically_derived]
-            impl #krate::label::LabelGroupSet for #set_ident {
-                // TODO: fix these generics
-                type Group<'a> = #ident #ty_generics;
+                #(
+                    #dynamic_decodes;
+                )*
 
-                #cardinality_fns
-
-                fn encode(&self, value: Self::Group<'_>) -> Option<Self::Unique> {
-                    let mut mul = 1;
-                    let mut index = 0;
-
-                    #(
-                        index += #fixed_encodes * mul;
-                        mul *= #cardinalities;
-                    )*
-
-                    Some((
-                        index
-                        #(, #dynamic_encodes)*
-                    ))
-                }
-
-                fn decode(&self, value: &Self::Unique) -> Self::Group<'_> {
-                    let (index #(, #dynamic_indices)*) = *value;
-
-                    #(
-                        let card = #cardinalities;
-                        let (index, index1) = (index / card, index % card);
-                        #fixed_decodes;
-                    )*
-
-                    debug_assert_eq!(index, 0);
-
-                    #(
-                        #dynamic_decodes;
-                    )*
-
-                    Self::Group {
-                        #(#field_names,)*
-                    }
+                Self::Group {
+                    #(#field_names,)*
                 }
             }
         })
